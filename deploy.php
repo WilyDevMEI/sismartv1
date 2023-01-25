028d2cefb084bb9af90ee08e881b251dfa01bf28
Nipe Setiawan Harefa <nipeharefa@gmail.com>
2023-01-25 23:41:47 +0700

    remove vendor

---
+++ deploy.php
@@ -0,0 +1,22 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'git@github.com:WilyDevMEI/sismartv1.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('')
+    ->set('remote_user', 'deployer')
+    ->set('deploy_path', '~/sismartv1');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
